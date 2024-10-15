06a3070b99f707b013eecb453fa84cb556c67d2d
rjaniak <rajkonkret660@gmail.com>
2024-10-15 13:54:18 +0200

    [2.17] add fun_17_ge

---
+++ day2/fun17.py
@@ -0,0 +1,11 @@
+from functools import reduce
+
+transactions = [
+    {"id": 1, "type": "income", "amount": 1000, "currency": "USD"},
+    {"id": 2, "type": "expense", "amount": 200, "currency": "USD"},
+    {"id": 3, "type": "income", "amount": 500, "currency": "USD"},
+    {"id": 4, "type": "expense", "amount": 300, "currency": "USD"},
+    {"id": 5, "type": "income", "amount": 700, "currency": "USD"},
+    {"id": 6, "type": "expense", "amount": 400, "currency": "EUR"},
+    {"id": 7, "type": "income", "amount": 100, "currency": "EUR"},
+]
