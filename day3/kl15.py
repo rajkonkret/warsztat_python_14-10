9c9aec5c9264eabad95b00c2736f60bf0a32b465
rjaniak <rajkonkret660@gmail.com>
2024-10-16 14:26:34 +0200

    [3.15] kl15 pickle

---
+++ day3/kl15.py
@@ -0,0 +1,32 @@
+# class Person:
+#     def __init__(self, first_name, last_name, id):
+#         self.first_name = first_name
+#         self.last_name = last_name
+#         self.id = id
+#
+#
+# p1 = Person("Jan", "Kowalski", 1)
+# print(p1) # <__main__.Person object at 0x0000020ACA4E57C0>
+import pickle
+from dataclasses import dataclass
+
+
+@dataclass
+class Person:
+    first_name: str
+    last_name: str
+    id: int
+
+
+p2 = Person("Jan", "Kowalski", 1)
+print(p2)  # Person(first_name='Jan', last_name='Kowalski', id=1)
+
+p3 = Person("Maciej", 'Nowak', 2)
+print(p3)  # Person(first_name='Maciej', last_name='Nowak', id=2)
+
+people = [p2, p3]
+print(
+    people)  # [Person(first_name='Jan', last_name='Kowalski', id=1), Person(first_name='Maciej', last_name='Nowak', id=2)]
+
+with open("dane.pickle", "wb") as stream:
+    pickle.dump(people, stream)
